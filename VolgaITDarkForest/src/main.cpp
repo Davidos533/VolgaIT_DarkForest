f47cd272318e964000e59bdfce628fe4b7a1d61d
Davidos533 <medvedkov29@gmail.com>
2022-05-23 21:18:04 +0300

    fix free resources if main.cpp
    add icon
    change error message

--- VolgaITDarkForest/src/main.cpp
+++ VolgaITDarkForest/src/main.cpp
@@ -2,26 +2,38 @@
 
 #include <cstdio>
 
+void freeMazeResources(architecture::Maze* maze);
+
 int main(int argc, const char* argv[])
 {
 	std::cerr << "place file with maze data near:\n" << argv[0] <<"\nand press any key...\n";
 
 	getchar();
 
-	architecture::Maze* maze = new architecture::Maze();
+	architecture::Maze* maze = nullptr;
 
 	try
 	{
+		maze = new architecture::Maze();
+
 		maze->startSearchingMeetPath();
 	}
 	catch (std::runtime_error error)
 	{
-		std::cerr << error.what() << "\n";
+		std::cerr << "program fatal error =(" << "\n";
 	}
 	
-	delete maze;
-	
+	freeMazeResources(maze);
+
 	getchar();
 
 	return 0;
+}
+
+void freeMazeResources(architecture::Maze* maze)
+{
+	if (maze != nullptr)
+	{
+		delete maze;
+	}
 }
